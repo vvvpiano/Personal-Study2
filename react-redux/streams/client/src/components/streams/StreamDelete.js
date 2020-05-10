069aba90e82fb854e2ab16bf3a4915f1724bd798
vvvpiano <vvvpiano60@gmail.com>
2020-05-11 02:01:13 +0900

    react portal, react fragment

--- react-redux/streams/client/src/components/streams/StreamDelete.js
+++ react-redux/streams/client/src/components/streams/StreamDelete.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import Modal from '../Modal';
 
 const StreamDelete = () => {
+    const actions = (
+        <React.Fragment>
+            <button className="ui button negative">Delete</button>
+            <button className="ui button">Cancel</button>
+        </React.Fragment>
+    );
     return (
-        <div>StreamDelete</div>
+        <div>
+            StreamDelete
+            <Modal
+                title="Delete Stream"
+                content="
+                Are you sure you want to delete this stream?"
+                actions={actions}
+            />
+        </div>
     )
 }
 
