2fb73f66dce76a91abd6e0f301ff17a377b2c156
vvvpiano <vvvpiano60@gmail.com>
2020-05-12 15:55:06 +0900

    React Portals finished

--- react-redux/streams/client/src/actions/index.js
+++ react-redux/streams/client/src/actions/index.js
@@ -42,7 +42,6 @@ export const fetchStream = (id) => async dispatch => {
 
     dispatch({ type: FETCH_STREAM, payload: response.data });
 };
-
 export const editStream = (id, formValues) => async dispatch => {
     const response = await streams.patch(`/streams/${id}`, formValues);
 
@@ -54,4 +53,5 @@ export const deleteStream = (id) => async dispatch => {
     await streams.delete(`/streams/${id}`);
 
     dispatch({ type: DELETE_STREAM, payload: id });
+    history.push('/');
 }
